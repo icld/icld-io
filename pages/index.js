9509254f06fc8f5917d5b48c5842b7b5028d9675
icld <ian@icldesign.com>
2021-08-16 18:09:41 -0700

    add home page animation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import Modal from 'react-modal';
 
@@ -8,6 +9,7 @@ Modal.setAppElement('#__next');
 
 export default function Home() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  // const [display ]
   return (
     <>
       <Head>
@@ -17,16 +19,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.container}>
-        <card className={styles.mainCard} onClick={() => setModalIsOpen(true)}>
-          <h1>Welcome to icld.io</h1>
+        <card
+          className={styles.mainCard}
+          //  onClick={() => setModalIsOpen(true)}
+        >
+          <h1 className={styles.title}>
+            <span className={styles.titleA}>Welcome to icld.io</span>
+            <span className={styles.titleB}>
+              <Link href='/about'>
+                <a>Click to learn more</a>
+              </Link>
+            </span>
+          </h1>
 
-          <p>
+          <p className={styles.description}>
             Hi! I am a Portland, Oregon based freelance programmer with a
             background in design and project management.
           </p>
         </card>
       </main>
-      <Modal
+      {/* <Modal
         isOpen={modalIsOpen}
         className={styles.Modal}
         overlayClassName={styles.Overlay}
@@ -34,7 +46,7 @@ export default function Home() {
         shouldCloseOnOverlayClick={true}
       >
         I'm a modal
-      </Modal>
+      </Modal> */}
     </>
   );
 }
