9185029b14efd11ecf49e67a9a5610c5db4a05a1
icld <ian@icldesign.com>
2021-08-17 14:25:50 -0700

    adout about card

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,11 +3,16 @@ import Link from 'next/link';
 import router, { useRouter } from 'next/router';
 // import styles from './About.module.css';
 import styles from '../../styles/Home.module.css';
+import aboutStyles from './About.module.css';
 
 export default () => (
   <>
     <main className={styles.container}>
-      <h1>About Page</h1>
+      <div className={aboutStyles.aboutMain}>
+        <div className={aboutStyles.aboutInner}>
+          <h1>About Page</h1>
+        </div>
+      </div>
     </main>
   </>
 );
