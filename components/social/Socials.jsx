42edbdbffa2f56ea6dc3a3fb82d87a21080911cb
icld <ian@icldesign.com>
2021-08-17 13:54:03 -0700

    fix social hover behavior

--- components/social/Socials.jsx
+++ components/social/Socials.jsx
@@ -4,16 +4,20 @@ import GitHub from './GitHub';
 import Email from './Email';
 import LinkedIn from './LinkedIn';
 
+import styles from '../../styles/Home.module.css';
 import socialStyles from './Social.module.css';
 
 export default function () {
   return (
-    <div className={socialStyles.mainSocials}>
-      <GitHub />
-      <Instagram />
-      <Twitter />
-      <LinkedIn />
-      <Email />
+    <div className=''>
+      <div className={socialStyles.overlay} />
+      <div className={socialStyles.mainSocials}>
+        <GitHub />
+        <Instagram />
+        <Twitter />
+        <LinkedIn />
+        <Email />
+      </div>
     </div>
   );
 }
