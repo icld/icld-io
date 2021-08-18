82171be20166fc350cdcd001bc767b7482e72fdd
icld <ian@icldesign.com>
2021-08-18 06:42:12 -0700

    fix post

--- components/Contact/Form/Form.jsx
+++ components/Contact/Form/Form.jsx
@@ -2,7 +2,50 @@ import { Form, Button } from 'react-bootstrap';
 import styles from './Form.module.css';
 export default function () {
   return (
-    <Form method='Post' data-netlify='true' name='contact' action='/success'>
+    // <Form
+    //   className={styles.form}
+    //   name='contact'
+    //   action='/success'
+    //   method='POST'
+    //   data-netlify='true'
+    // >
+    //   <input type='hidden' name='form-name' value='contact' />
+    //   <p
+    //     style={{
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       width: '50%',
+    //     }}
+    //   >
+    //     <label htmlFor='yourname'>Your Name:</label> <br />
+    //     <input
+    //       className={styles.firstName}
+    //       type='text'
+    //       placeholder='Your first name here'
+    //       name='firstname'
+    //       id='firstname'
+    //     />
+    //     <input
+    //       className={styles.lastName}
+    //       type='text'
+    //       name='name'
+    //       id='yourname'
+    //     />
+    //   </p>
+    //   <p>
+    //     <label htmlFor='youremail'>Your Email:</label> <br />
+    //     <input type='email' name='email' id='youremail' />
+    //   </p>
+    //   <p>
+    //     <label htmlFor='yourmessage'>Message:</label> <br />
+    //     <textarea name='message' id='yourmessage'></textarea>
+    //   </p>
+    //   <p>
+    //     <button type='submit'>Send</button>
+    //   </p>
+    // </Form>
+
+    <Form method='POST' data-netlify='true' name='contact' action='/success'>
       <Form.Group>
         <Form.Label>Your name</Form.Label>
         <Form.Control type='text' placeholder='What shall we call you?' />
