9d766bef081d6fd2df9a702a8f747e34e88a5269
icld <ian@icldesign.com>
2021-08-18 06:38:58 -0700

    add bootstrap form

--- components/Contact/Form/Form.jsx
+++ components/Contact/Form/Form.jsx
@@ -1,48 +1,27 @@
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import styles from './Form.module.css';
 export default function () {
   return (
-    <form
-      className={styles.form}
-      name='contact'
-      action='/success'
-      method='POST'
-      data-netlify='true'
-    >
-      <input type='hidden' name='form-name' value='contact' />
-      <p
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '50%',
-        }}
-      >
-        <label htmlFor='yourname'>Your Name:</label> <br />
-        <input
-          className={styles.firstName}
-          type='text'
-          placeholder='Your first name here'
-          name='firstname'
-          id='firstname'
-        />
-        <input
-          className={styles.lastName}
-          type='text'
-          name='name'
-          id='yourname'
-        />
-      </p>
-      <p>
-        <label htmlFor='youremail'>Your Email:</label> <br />
-        <input type='email' name='email' id='youremail' />
-      </p>
-      <p>
-        <label htmlFor='yourmessage'>Message:</label> <br />
-        <textarea name='message' id='yourmessage'></textarea>
-      </p>
-      <p>
-        <button type='submit'>Send</button>
-      </p>
-    </form>
+    <Form method='Post' data-netlify='true' name='contact' action='/success'>
+      <Form.Group>
+        <Form.Label>Your name</Form.Label>
+        <Form.Control type='text' placeholder='What shall we call you?' />
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type='email' placeholder='Enter email' />
+        {/* <Form.Text className='text-muted'>
+          I will only use this to write back to you
+        </Form.Text> */}
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Your message:</Form.Label>
+        <Form.Control as='textarea' rows={5} />
+      </Form.Group>
+
+      <Button variant='primary' type='submit' placeholder=''>
+        Send it off!
+      </Button>
+    </Form>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Home.module.css';
 import Header from '../components/Header/Header';
 import Image from 'next/image';
